7dc1b8d4c24d0f00fb3ff30f1249713c353ec094
Abubakar Sadiq Khalid <61843341+skha180@users.noreply.github.com>
2025-12-12 17:17:56 +0100

    Add www.herblife.world to ALLOWED_HOSTS

--- herb_project/settings.py
+++ herb_project/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-lq=q@z%_+)(0!^^v66$+k&6s2_s#y*+pnj=#@lvnc=g1(=yg@=
 DEBUG = True   
 
 # 👉 Add Render domain here after deployment (example):
-ALLOWED_HOSTS = ['herblife.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['herblife.onrender.com', 'localhost', '127.0.0.1','www.herblife.world']
 
 # Application definition
 INSTALLED_APPS = [
